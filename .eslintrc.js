d51660128bd4163109d1f12c5ff08d50a3e062e9
xuliangzhan <xu_liangzhan@163.com>
2019-02-16 22:32:12 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   'extends': [
     'plugin:vue/essential',
-    'eslint:recommended'
+    'standard'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',

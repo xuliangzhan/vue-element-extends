d1ff16af7a1dac30cb45a1bc925c0394093b5aff
xuliangzhan <xu_liangzhan@163.com>
2019-02-17 17:45:00 +0800

    update

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   'presets': [
-    '@vue/app'
+    ['@vue/app', {
+      polyfills: [
+        'es6.promise',
+        'es6.symbol',
+        'es6.array.iterator',
+        'es6.object.assign'
+      ]
+    }]
   ],
   'plugins': [
     [

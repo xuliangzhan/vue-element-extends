7031ec1694711a273dc1bb2923e1f9993d64ae81
xuliangzhan <xu_liangzhan@163.com>
2019-04-06 17:15:11 +0800

    update 1.0

--- examples/plugins/element-extends.js
+++ examples/plugins/element-extends.js
@@ -2,5 +2,5 @@ import Vue from 'vue'
 import Editable from '../../packages/editable/src/editable.vue'
 import EditableColumn from '../../packages/editable/src/editable-column.vue'
 
-Vue.component(Editable.name, Editable)
-Vue.component(EditableColumn.name, EditableColumn)
+Vue.component('ElEditable', Editable)
+Vue.component('ElEditableColumn', EditableColumn)

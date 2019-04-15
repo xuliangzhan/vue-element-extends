57299571ebd69fbd5b2ff87416876ad2d92e0bef
xuliangzhan <xu_liangzhan@163.com>
2019-04-16 00:06:44 +0800

    update

--- docs/static/js/mock.js
+++ docs/static/js/mock.js
@@ -1,21 +1,30 @@
 onmessage = function (event) {
   var list = []
   var currTime = Date.now()
+  var fullIndex = 0
   var size = event.data.size
-  for (var index = 0; index < size; index++) {
-    currTime += 5000
-    list.push({
-      _index: index + 1,
-      id: 10000 + index,
-      name: 'name_' + index,
-      date: currTime,
-      sex: index % 3 ? '0' : '1',
-      age: index % 4 === 0 ? 30 : index % 3 === 0 ? 28 : index % 2 === 0 ? 26 : 24,
-      region: index % 4 === 0 ? [19, 199, 1773] : index % 3 === 0 ? [9, 73, 719] : [1, 1, 5],
-      rate: index % 4 === 0 ? 4 : index % 3 === 0 ? 3 : index % 2 === 0 ? 2 : 1,
-      updateTime: currTime,
-      createTime: currTime
-    })
+  var setData = function () {
+    for (var index = 0; index < 2000; index++) {
+      currTime += 5000
+      fullIndex++
+      list.push({
+        _index: fullIndex,
+        id: 10000 + fullIndex,
+        name: 'name_' + fullIndex,
+        date: currTime,
+        sex: index % 3 ? '0' : '1',
+        age: index % 4 === 0 ? 30 : index % 3 === 0 ? 28 : index % 2 === 0 ? 26 : 24,
+        region: index % 4 === 0 ? [19, 199, 1773] : index % 3 === 0 ? [9, 73, 719] : [1, 1, 5],
+        rate: index % 4 === 0 ? 4 : index % 3 === 0 ? 3 : index % 2 === 0 ? 2 : 1,
+        updateTime: currTime,
+        createTime: currTime
+      })
+    }
+    if (fullIndex >= size) {
+      self.postMessage({ size: size, list: list })
+    } else {
+      setTimeout(setData, 50)
+    }
   }
-  self.postMessage({ size: size, list: list })
+  setData()
 }

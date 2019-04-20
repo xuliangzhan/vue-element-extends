7fec3efb3efe9f0f83b74d5eb01be50c69a20523
xuliangzhan <xu_liangzhan@163.com>
2019-04-20 18:06:11 +0800

    update

--- examples/views/editable/BigScroll1.vue
+++ examples/views/editable/BigScroll1.vue
@@ -28,15 +28,6 @@
       <elx-editable-column prop="age" label="年龄" min-width="200" :edit-render="{name: 'ElInputNumber', attrs: {min: 1, max: 200}}"></elx-editable-column>
       <elx-editable-column prop="region" label="地区" width="200" :edit-render="{name: 'ElCascader', attrs: {options: regionList}}"></elx-editable-column>
       <elx-editable-column prop="date" label="日期" width="220" :edit-render="{name: 'ElDatePicker', attrs: {type: 'datetime', format: 'yyyy年MM月dd日 HH时ss分mm秒'}}"></elx-editable-column>
-      <elx-editable-column prop="rate" label="评分" width="200" :edit-render="{type: 'visible'}">
-        <template v-slot:edit="scope">
-          <el-rate
-            v-model="scope.row.rate"
-            show-score
-            text-color="#ff9900">
-          </el-rate>
-        </template>
-      </elx-editable-column>
       <elx-editable-column prop="updateTime" label="更新时间" width="200" :formatter="formatterDate"></elx-editable-column>
       <elx-editable-column prop="createTime" label="创建时间" width="200" :formatter="formatterDate"></elx-editable-column>
       <elx-editable-column prop="name" label="名字" min-width="100" fixed="right" show-overflow-tooltip :edit-render="{name: 'ElInput'}"></elx-editable-column>

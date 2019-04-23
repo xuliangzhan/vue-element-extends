9b2a3c1c32169d0a71e119eca9f97787494260c0
xuliangzhan <xu_liangzhan@163.com>
2019-04-23 13:01:46 +0800

    update

--- examples/plugins/element.js
+++ examples/plugins/element.js
@@ -2,4 +2,5 @@ import Vue from 'vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(Element, { size: 'small', zIndex: 3000 })
+// Vue.use(Element, { size: 'small', zIndex: 3000 })
+Vue.use(Element)

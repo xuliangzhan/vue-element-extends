a3282a0229d884631280c87f15412dd84c48c5af
xuliangzhan <xu_liangzhan@163.com>
2021-03-30 11:18:35 +0800

    rc

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -9,5 +9,5 @@ function is (it) {
 
 if (!ADBLOCK && !CI && !DISABLE_OPENCOLLECTIVE) {
   console.log('\n\x1B[91m>\x1B[91m 注: 该组件 vue-element-extends 已废弃不再维护，不再建议使用！')
-  console.log('\x1B[93m>\x1B[92m 后续推荐使用: https://xuliangzhan_admin.gitee.io/vxe-table/#/donation/api\n')
+  console.log('\x1B[93m>\x1B[92m 推荐使用: https://xuliangzhan_admin.gitee.io/vxe-table/\n')
 }
